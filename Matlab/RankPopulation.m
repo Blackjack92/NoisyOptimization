0ba4dad4158be13bb33fed7834aa42819cf01632
Kevin <kevin.wallis@hotmail.com>
2015-07-25 10:21:58 +0200

    Added a function for ranking the population.

---
+++ Matlab/RankPopulation.m
@@ -0,0 +1,42 @@
+% Name:   RankPopulation
+% Author: Kevin Wallis
+%--------------------------------------------------------------------------
+%| Input parameters                                                       |
+%--------------------------------------------------------------------------
+%| Name                | Type        | Description                        |
+%--------------------------------------------------------------------------
+%| offspringPopulation | [M x N]     | Offspring population which should  |
+%|                     |             | be ranked                          |
+%| ordering            | String      | Ordering type                      |
+%--------------------------------------------------------------------------
+%| Output parameters                                                      |
+%--------------------------------------------------------------------------
+%| Name                | Type        | Description                        |
+%--------------------------------------------------------------------------
+%| raning               | [M x N]    | Ranking of the population          |
+%--------------------------------------------------------------------------
+function [ranking] = RankPopulation(offspringPopulation, ordering)
+    % Initialize output
+    ranking = [];
+
+    lambda = size(offspringPopulation, 2);
+    for l=1:lambda
+        indexBestValue = FindBest(offspringPopulation);
+        ranking(l) = offspringPopulation(indexBestValue);
+        offspringPopulation(indexBestValue).f = inf; 
+    end
+end
+
+function [index] = FindBest(offspringPopulation)
+    % Initialize parameters    
+    lambda = size(offspringPopulation, 2);
+    bestValue = offspringPopulation(1).f;
+    index = 1;
+    
+    for l=2:lambda
+        if (offspringPopulation(l).f < bestValue)
+            bestValue = offspringPopulation(l).f;
+            index = l;
+        end
+    end
+end
